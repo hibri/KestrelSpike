ac6e7279d90e8dca476a7ad910d603df6c6bd247
Hibri Marzook <hibri@hibri.net>
2020-08-24 22:35:40 +0100

    wip

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿#define DefaultBuilder
+// Define any of the following for the scenarios described in the Kestrel topic:
+// DefaultBuilder Limits TCPSocket UnixSocket FileDescriptor Port0 SyncIO
+// The following require an X.509 certificate:
+// TCPSocket UnixSocket FileDescriptor Limits
+
+using System;
+using System.Net;
+using Microsoft.Extensions.Hosting;
+
+
+namespace KestrelSpike
+{
+    public class Program
+    {
+
+  
+        public static void Main(string[] args)
+        {
+            
+        }
+
+
+        private void buildServer()
+        {
+            var hostBuilder = new HostBuilder();
+            
+        }
+
+        
+    }
+}
